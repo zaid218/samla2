198ea3c2bf6cb4e64bff33e159b5a8a581bd88ad
Mohd Zaid Khan <zaidk9620@gmail.com>
2024-02-07 09:59:05 +0530

    styling added

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import UserList from './components/UserList/UserList';
 import SearchUser from './components/SearchUser/SearchUser';
-import store from './store/store';  // Assuming you named your store configuration file as 'store.js'
+import store from './store/store';
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">User List</Link>
-              </li>
-              <li>
-                <Link to="/search">Search User</Link>
-              </li>
-            </ul>
-          </nav>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}> 
+          <div style={{ backgroundColor: '#f5f5f5', padding: '20px', borderRadius: '5px' }}> 
+            <nav style={{ display: 'flex', justifyContent: 'space-between', width: '500px' }}>
+              <Link to="/">
+                <button>User List</button>
+              </Link>
+              <Link to="/search">
+                <button>Search User</button>
+              </Link>
+            </nav>
 
-          <Routes>
-            <Route path="/" element={<UserList />} />
-            <Route path="/search" element={<SearchUser />} />
-          </Routes>
+            <Routes>
+              <Route path="/" element={<UserList />} />
+              <Route path="/search" element={<SearchUser />} />
+            </Routes>
+          </div>
         </div>
       </Router>
     </Provider>
